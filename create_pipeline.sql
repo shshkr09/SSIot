a72747d7484175929e2492981480816d7afaf18d
shshkr09 <63920849+shshkr09@users.noreply.github.com>
2022-07-19 20:30:45 +0530

    add
    add csv

--- create_pipeline.sql
+++ create_pipeline.sql
@@ -18,9 +18,7 @@ FIELDS TERMINATED BY ',' ENCLOSED BY '' ESCAPED BY '\\'
 LINES TERMINATED BY '\n' STARTING BY '';
 
 create or replace pipeline meter_to_transformer1
-AS LOAD DATA S3 's3://singlestore-test/mt.csv'
-CONFIG '{\"region\":\"us-west-1\"}'
-CREDENTIALS '{"aws_access_key_id": "AKIAVL66XGD5RLFJQC6F", "aws_secret_access_key": "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"}'
+AS LOAD DATA FS '$HOME/SSIot/mt.csv'
 SKIP DUPLICATE KEY ERRORS
 INTO TABLE `meter_to_transformer`
 FIELDS TERMINATED BY '\t' ENCLOSED BY '"';
